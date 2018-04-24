e907fb3ac5dbe82c8c62d39a21351b5ac850e336
Lionel Henry <lionel.hry@gmail.com>
2018-04-24 02:03:18 +0200

    Don't patch harfbuzz except on Solaris

--- tools/make-harfbuzz.sh
+++ tools/make-harfbuzz.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -z $HB_VERSION ]; then
-    HB_VERSION=1.7.6;
+    HB_VERSION=1.7.6
 fi
 
 HB="harfbuzz-${HB_VERSION}"
@@ -10,21 +10,19 @@ HB_ARCHIVE="downloads/${HB}.tar.bz2"
 
 if [ ! -e ${HB_ARCHIVE} ]; then
     echo "Please run 'make ${HB_ARCHIVE}'"
-    exit 1;
+    exit 1
 fi
 
-
-# Solaris 10 `patch` does not support modern diffs
-if [ $(uname) = "SunOS" ]; then
-    PATCH="gpatch";
-else
-    PATCH="patch";
-fi
 
 bunzip2 < ${HB_ARCHIVE} | tar xf -
-cd ${HB}
-${PATCH} < ../harfbuzz-pkgconfig.diff
-cd ..
+
+# Use gpatch because Solaris 10 `patch` does not support modern diffs.
+# The patch ensures the configure script detects FreeType as R-hub has
+# a very old version of pkg-config that does not seem to work with
+# modern .pc files.
+if [ $(uname) = "SunOS" ]; then
+    (cd ${HB}; gpatch < ../harfbuzz-pkgconfig.diff)
+fi
 
 tar cf - ${HB} | gzip -9 > ${HB}.tar.gz
 rm -rf ${HB}
