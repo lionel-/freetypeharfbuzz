833e1eee246a51ae1e7affc1340819eeb2590d14
Lionel Henry <lionel.hry@gmail.com>
2018-04-26 18:59:20 +0200

    Extract validate_string_info_inputs()

--- src/string-info.c
+++ src/string-info.c
@@ -84,23 +84,28 @@ const char* metrics_names[] = {
   "width", "ascender", "descender", "linegap", ""
 };
 
-SEXP string_info(SEXP string, SEXP font_size, SEXP font_file) {
+validate_string_info_inputs(SEXP* string, SEXP* font_size, SEXP* font_file) {
   int n_protect = 0;
 
-  if (TYPEOF(string) != STRSXP || Rf_length(string) != 1) {
+  if (TYPEOF(*string) != STRSXP || Rf_length(*string) != 1) {
     Rf_errorcall(R_NilValue, "`string` must be a length 1 character vector");
   }
-  if (TYPEOF(font_file) != STRSXP || Rf_length(font_file) != 1) {
+  if (TYPEOF(*font_file) != STRSXP || Rf_length(*font_file) != 1) {
     Rf_errorcall(R_NilValue, "`font_file` must be a length 1 character vector");
   }
 
-  if (TYPEOF(font_size) == REALSXP) {
-    font_size = PROTECT(Rf_coerceVector(font_size, INTSXP));
+  if (TYPEOF(*font_size) == REALSXP) {
+    *font_size = PROTECT(Rf_coerceVector(*font_size, INTSXP));
     ++n_protect;
   }
-  if (TYPEOF(font_size) != INTSXP || Rf_length(font_size) != 1) {
+  if (TYPEOF(*font_size) != INTSXP || Rf_length(*font_size) != 1) {
     Rf_errorcall(R_NilValue, "`font_size` must be a length 1 numeric vector");
   }
+  return n_protect;
+}
+
+SEXP string_info(SEXP string, SEXP font_size, SEXP font_file) {
+  int n_protect = validate_string_info_inputs(&string, &font_size, &font_file);
 
   const char* text = Rf_translateCharUTF8(STRING_ELT(string, 0));
   const char* font_path = CHAR(STRING_ELT(font_file, 0));
