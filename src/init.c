fe6a65f390c47b8abd2c168ad5bd6773adf1a2c4
Lionel Henry <lionel.hry@gmail.com>
2018-04-28 10:12:04 +0200

    Add C callables for calc_string_info() and get_font_info()

--- src/init.c
+++ src/init.c
@@ -11,22 +11,25 @@ extern SEXP string_info(SEXP, SEXP, SEXP);
 extern SEXP string_width(SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"freetypeharfbuzz_font_info", (DL_FUNC) &font_info, 2},
-    {"freetypeharfbuzz_library_load", (DL_FUNC) &library_load, 0},
-    {"freetypeharfbuzz_library_unload", (DL_FUNC) &library_unload, 0},
-    {"freetypeharfbuzz_string_info", (DL_FUNC) &string_info, 3},
-    {"freetypeharfbuzz_string_width", (DL_FUNC) &string_width, 3},
-    {NULL, NULL, 0}
+    { "freetypeharfbuzz_font_info",      (DL_FUNC) &font_info,      2 },
+    { "freetypeharfbuzz_library_load",   (DL_FUNC) &library_load,   0 },
+    { "freetypeharfbuzz_library_unload", (DL_FUNC) &library_unload, 0 },
+    { "freetypeharfbuzz_string_info",    (DL_FUNC) &string_info,    3 },
+    { "freetypeharfbuzz_string_width",   (DL_FUNC) &string_width,   3 },
+    { NULL, NULL, 0 }
 };
 
 
+#include "font-info.h"
 #include "string-info.h"
 
 void R_init_freetypeharfbuzz(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 
+    R_RegisterCCallable("freetypeharfbuzz", "freetypeharfbuzz_calc_string_info",  (DL_FUNC) &calc_string_info);
     R_RegisterCCallable("freetypeharfbuzz", "freetypeharfbuzz_calc_string_width", (DL_FUNC) &calc_string_width);
+    R_RegisterCCallable("freetypeharfbuzz", "freetypeharfbuzz_get_font_info",     (DL_FUNC) &get_font_info);
 }
 
 
